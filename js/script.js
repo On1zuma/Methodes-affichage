1df1dce524f028518f32453cab774376e8f5412f
On1zuma <wassi.mess@hotmail.com>
2023-01-30 19:05:45 +0100

    feat: new components (arrow + footer)

--- js/script.js
+++ js/script.js
@@ -1,64 +1,53 @@
+const loader = document.querySelector(".loader"); //effet de chargement
 
-
-const loader = document.querySelector('.loader');//effet de chargement 
-
-window.addEventListener('load', () =>{
-      loader.classList.add('fondu-out')//effet fin de chargement "sur le css"
+window.addEventListener("load", () => {
+  loader.classList.add("fondu-out"); //effet fin de chargement "sur le css"
+  setTimeout(() => {}, 500);
 });
 
+const accordionItemHeaders = document.querySelectorAll(
+  ".accordion-item-header"
+);
 
+accordionItemHeaders.forEach((accordionItemHeader) => {
+  accordionItemHeader.addEventListener("click", (event) => {
+    // Programme JAVA SCRIPT ANIMATION DU MENU DEROULANT
+    //pour avoir seulement un onglet qui defile , le maximum
 
-const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
-
-accordionItemHeaders.forEach(accordionItemHeader => {
-  accordionItemHeader.addEventListener("click", event => {
-    // Programme JAVA SCRIPT ANIMATION DU MENU DEROULANT 
-    //pour avoir seulement un onglet qui defile , le maximum 
-    
     // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
     // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
     //   currentlyActiveAccordionItemHeader.classList.toggle("active");
     //  currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-  //   }
-  //pour avoir seulement un onglet qui defile , le maximum 
+    //   }
+    //pour avoir seulement un onglet qui defile , le maximum
 
     accordionItemHeader.classList.toggle("active");
     const accordionItemBody = accordionItemHeader.nextElementSibling;
-    if(accordionItemHeader.classList.contains("active")) {
+    if (accordionItemHeader.classList.contains("active")) {
       accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    }
-    else {
+    } else {
       accordionItemBody.style.maxHeight = 0;
     }
   });
 });
 
-
-
-
-var timelineSwiper = new Swiper ('.timeline .swiper-container', {
-  direction: 'vertical',
+var timelineSwiper = new Swiper(".timeline .swiper-container", {
+  direction: "vertical",
   loop: false,
   speed: 1600,
-  pagination: '.swiper-pagination',
+  pagination: ".swiper-pagination",
   paginationBulletRender: function (swiper, index, className) {
-    var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
-    return '<span class="' + className + '">' + year + '</span>';
+    var year = document
+      .querySelectorAll(".swiper-slide")
+      [index].getAttribute("data-year");
+    return '<span class="' + className + '">' + year + "</span>";
   },
   paginationClickable: true,
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev',
+  nextButton: ".swiper-button-next",
+  prevButton: ".swiper-button-prev",
   breakpoints: {
     768: {
-      direction: 'horizontal',
-    }
-  }
+      direction: "horizontal",
+    },
+  },
 });
-
-
-
-
-
-
-
-
--- js/scroll.js
+++ js/scroll.js
@@ -1,21 +1,54 @@
+jQuery(function () {
+  $("#scroll_to_top").css("transition", "visibility 0s, opacity 0.5s linear");
+  $("#scroll_to_top").css("visibility", "hidden");
+  $("#scroll_to_top").css("opacity", "0");
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {
+        $("#scroll_to_top").css("right", "17px");
+        $("#scroll_to_top").css("visibility", "visible");
+        $("#scroll_to_top").css("opacity", "0.8");
+        $("#scroll_to_top").css(
+          "transition",
+          "visibility 0s, opacity 0.5s linear"
+        );
+      } else {
+        //  $('#scroll_to_top').css('display','none');
+        $("#scroll_to_top").css(
+          "transition",
+          "visibility 0s, opacity 0.5s linear"
+        );
+        $("#scroll_to_top").css("visibility", "hidden");
+        $("#scroll_to_top").css("opacity", "0");
+      }
+    });
+  });
+});
 
-jQuery(function(){                    
-  $('#scroll_to_top').css('transition','visibility 0s, opacity 0.5s linear');
-  $('#scroll_to_top').css('visibility','hidden');
-  $('#scroll_to_top').css('opacity','0')
-      $(function () {
-          $(window).scroll(function () {
-              if ($(this).scrollTop() > 200 ) { 
-                  $('#scroll_to_top').css('right','17px');
-                  $('#scroll_to_top').css('visibility','visible');
-                  $('#scroll_to_top').css('opacity','0.8');
-                  $('#scroll_to_top').css('transition','visibility 0s, opacity 0.5s linear');
-              } else { 
-                //  $('#scroll_to_top').css('display','none');
-                      $('#scroll_to_top').css('transition','visibility 0s, opacity 0.5s linear');
-                      $('#scroll_to_top').css('visibility','hidden');
-                      $('#scroll_to_top').css('opacity','0');
-              }
-          });
-      });
+//arrow components
+fetch("components/arrow.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.querySelector("#arrowToTop").innerHTML = data;
+  });
+
+//arrow components for index
+fetch("/html/components/arrow.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.querySelector("#arrowToTopIndex").innerHTML = data;
+  });
+
+// footer components index
+fetch("components/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.querySelector("#footerContainer").innerHTML = data;
+  });
+
+// footer components index
+fetch("/html/components/footer.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.querySelector("#footerContainerIndex").innerHTML = data;
   });
