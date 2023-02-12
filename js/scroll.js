aa44165733566e09bb3f657dae256907aa183bef
On1zuma <wassi.mess@hotmail.com>
2023-02-12 22:32:19 +0100

    fix: arrow to the top updated

--- js/scroll.js
+++ js/scroll.js
@@ -1,36 +1,21 @@
 $(window).load(function () {
-  jQuery(function () {
-    $("#scroll_to_top").css("transition", "visibility 0s, opacity 0.5s linear");
-    $("#scroll_to_top").css("visibility", "hidden");
-    $("#scroll_to_top").css("opacity", "0");
-    $(function () {
-      $(window).scroll(function () {
-        if ($(this).scrollTop() > 200) {
-          $("#scroll_to_top").css("right", "17px");
-          $("#scroll_to_top").css("visibility", "visible");
-          $("#scroll_to_top").css("opacity", "0.8");
-          $("#scroll_to_top").css(
-            "transition",
-            "visibility 0s, opacity 0.5s linear"
-          );
-        } else {
-          //  $('#scroll_to_top').css('display','none');
-          $("#scroll_to_top").css(
-            "transition",
-            "visibility 0s, opacity 0.5s linear"
-          );
-          $("#scroll_to_top").css("visibility", "hidden");
-          $("#scroll_to_top").css("opacity", "0");
-        }
-      });
-    });
-  });
-
   //arrow components
   fetch("components/arrow.html")
     .then((response) => response.text())
     .then((data) => {
       document.querySelector("#arrowToTop").innerHTML = data;
+
+      let scrollToTop = document.getElementById("scroll_to_top");
+      window.onscroll = function () {
+        if (
+          document.body.scrollTop > 20 ||
+          document.documentElement.scrollTop > 20
+        ) {
+          scrollToTop.style.display = "block";
+        } else {
+          scrollToTop.style.display = "none";
+        }
+      };
     });
 
   //arrow components for index
@@ -38,6 +23,18 @@ $(window).load(function () {
     .then((response) => response.text())
     .then((data) => {
       document.querySelector("#arrowToTopIndex").innerHTML = data;
+
+      let scrollToTop = document.getElementById("scroll_to_top");
+      window.onscroll = function () {
+        if (
+          document.body.scrollTop > 20 ||
+          document.documentElement.scrollTop > 20
+        ) {
+          scrollToTop.style.display = "block";
+        } else {
+          scrollToTop.style.display = "none";
+        }
+      };
     });
 
   // footer components
