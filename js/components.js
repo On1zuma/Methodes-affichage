7adf2b98cb4b09e18df704172ad83d82507897eb
Wassim <96482486+On1zuma@users.noreply.github.com>
2023-02-14 23:51:02 +0100

    Vuejs implementation (#4)
    * feat: Implementation of Vue and update of the database
    
    * fix: arrow to the top updated
    
    * refactoring : from arrow to components
    
    * refactoring : code cleaning

---
+++ js/components.js
@@ -0,0 +1,53 @@
+$(window).load(function () {
+  //arrow components
+  fetch("components/arrow.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.querySelector("#arrowToTop").innerHTML = data;
+
+      let scrollToTop = document.getElementById("scroll_to_top");
+      window.onscroll = function () {
+        if (
+          document.body.scrollTop > 20 ||
+          document.documentElement.scrollTop > 20
+        ) {
+          scrollToTop.style.display = "block";
+        } else {
+          scrollToTop.style.display = "none";
+        }
+      };
+    });
+
+  //arrow components for index
+  fetch("/html/components/arrow.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.querySelector("#arrowToTopIndex").innerHTML = data;
+
+      let scrollToTop = document.getElementById("scroll_to_top");
+      window.onscroll = function () {
+        if (
+          document.body.scrollTop > 20 ||
+          document.documentElement.scrollTop > 20
+        ) {
+          scrollToTop.style.display = "block";
+        } else {
+          scrollToTop.style.display = "none";
+        }
+      };
+    });
+
+  // footer components
+  fetch("components/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.querySelector("#footerContainer").innerHTML = data;
+    });
+
+  //nav components
+  fetch("components/nav.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.querySelector("#appnavbar").innerHTML = data;
+    });
+});
