0b9da282815e9d2f8195f38271452f7232240218
On1zuma <wassi.mess@hotmail.com>
2023-02-15 21:18:41 +0100

    refacto: code cleaning + css update

--- js/components.js
+++ js/components.js
@@ -1,53 +1,55 @@
 $(window).load(function () {
-  //arrow components
-  fetch("components/arrow.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.querySelector("#arrowToTop").innerHTML = data;
+  if (location.pathname === "/index.html") {
+    //arrow components for index
+    fetch("/html/components/arrow.html")
+      .then((response) => response.text())
+      .then((data) => {
+        document.querySelector("#arrowToTopIndex").innerHTML = data;
 
-      let scrollToTop = document.getElementById("scroll_to_top");
-      window.onscroll = function () {
-        if (
-          document.body.scrollTop > 20 ||
-          document.documentElement.scrollTop > 20
-        ) {
-          scrollToTop.style.display = "block";
-        } else {
-          scrollToTop.style.display = "none";
-        }
-      };
-    });
+        let scrollToTop = document.getElementById("scroll_to_top");
+        window.onscroll = function () {
+          if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+          ) {
+            scrollToTop.style.display = "block";
+          } else {
+            scrollToTop.style.display = "none";
+          }
+        };
+      });
+  } else {
+    //arrow components
+    fetch("components/arrow.html")
+      .then((response) => response.text())
+      .then((data) => {
+        document.querySelector("#arrowToTop").innerHTML = data;
 
-  //arrow components for index
-  fetch("/html/components/arrow.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.querySelector("#arrowToTopIndex").innerHTML = data;
+        let scrollToTop = document.getElementById("scroll_to_top");
+        window.onscroll = function () {
+          if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+          ) {
+            scrollToTop.style.display = "block";
+          } else {
+            scrollToTop.style.display = "none";
+          }
+        };
+      });
 
-      let scrollToTop = document.getElementById("scroll_to_top");
-      window.onscroll = function () {
-        if (
-          document.body.scrollTop > 20 ||
-          document.documentElement.scrollTop > 20
-        ) {
-          scrollToTop.style.display = "block";
-        } else {
-          scrollToTop.style.display = "none";
-        }
-      };
-    });
+    // footer components
+    fetch("components/footer.html")
+      .then((response) => response.text())
+      .then((data) => {
+        document.querySelector("#footerContainer").innerHTML = data;
+      });
 
-  // footer components
-  fetch("components/footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.querySelector("#footerContainer").innerHTML = data;
-    });
-
-  //nav components
-  fetch("components/nav.html")
-    .then((response) => response.text())
-    .then((data) => {
-      document.querySelector("#appnavbar").innerHTML = data;
-    });
+    //nav components
+    fetch("components/nav.html")
+      .then((response) => response.text())
+      .then((data) => {
+        document.querySelector("#appnavbar").innerHTML = data;
+      });
+  }
 });
